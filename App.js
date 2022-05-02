d3145c20264487b59dddc0532d04e5705d531fb2
Augustine Awuori <augutineawuroi95@gmail.com>
2022-05-02 15:17:45 +0300

    Create an Auth Navigator.

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import FeedScreen from './app/screens/LoginScreen'
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
   return (
-    <FeedScreen />
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
   );
 }
-// <View style={styles.container}>
-//   <Text>Open up App.js to start working on your app!</Text>
-//   <StatusBar style="auto" />
-// </View>
 
 const styles = StyleSheet.create({
   container: {
