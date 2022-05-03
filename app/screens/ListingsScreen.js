c8d66fa33cc6d2816985eace9cc08b87e1d6f2e7
Augustine Awuori <augutineawuroi95@gmail.com>
2022-05-03 17:53:05 +0300

    Handle errors when api call fails.

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,39 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import React, { useEffect, useState } from 'react'
+import { FlatList, StyleSheet } from 'react-native'
 
-import Card from "../components/Card";
-import colors from "../config/colors";
-import listingsApi from "../api/listings";
-import Screen from "../components/Screen";
+import Card from '../components/Card'
+import colors from '../config/colors'
+import Button from '../components/Button'
+import listingsApi from '../api/listings'
+import Screen from '../components/Screen'
+import AppText from '../components/AppText'
 
 function ListingsScreen({ navigation }) {
-  const [listings, setListings] = useState([]);
+  const [listings, setListings] = useState([])
+  const [error, setError] = useState([])
 
   useEffect(() => {
-    loadListings();
-  }, []);
+    loadListings()
+  }, [])
 
   const loadListings = async () => {
-    const { data } = await listingsApi.getListings();
-    setListings(data);
-  };
+    const { data, ok } = await listingsApi.getListings()
+    if (!ok) return setError(true)
+
+    setError(false)
+    setListings(data)
+  }
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <Button title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
         renderItem={({ item }) => (
           <Card
             title={item.title}
-            subTitle={"$" + item.price}
+            subTitle={'$' + item.price}
             imageUrl={item.images[0].url}
-            onPress={() => navigation.navigate("ListingDetails", item)}
+            onPress={() => navigation.navigate('ListingDetails', item)}
           />
         )}
       />
     </Screen>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -42,6 +54,6 @@ const styles = StyleSheet.create({
     paddingBottom: 0,
     backgroundColor: colors.light,
   },
-});
+})
 
-export default ListingsScreen;
+export default ListingsScreen
