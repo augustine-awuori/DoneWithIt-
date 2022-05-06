6ca9fe22907fdc41218349c7ef7a23527d7d2df6
Augustine Awuori <augutineawuroi95@gmail.com>
2022-05-06 16:45:00 +0300

    Add an overlay to the loading indicator.

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native'
 import LottieView from 'lottie-react-native';
+
+import colors from '../config/colors';
 
 export default function ActivityIndicator({ visible = false }) {
     if (!visible) return null;
 
-    return <LottieView autoPlay loop source={require('../assets/animations/loader.json')} />;
+    return (
+        <View style={styles.overlay}>
+            <LottieView
+                autoPlay
+                loop source={require('../assets/animations/loader.json')} />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    overlay: {
+        position: 'absolute',
+        backgroundColor: colors.white,
+        height: '100%',
+        opacity: .8,
+        width: '100%',
+        zIndex: 1
+    }
+})
