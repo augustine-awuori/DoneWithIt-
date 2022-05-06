3f1688584aa7a25d5a00d7effeb0f1a63729139b
Augustine Awuori <augutineawuroi95@gmail.com>
2022-05-06 17:05:25 +0300

    Get the push notification token.

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from '@expo/vector-icons/MaterialCommunityIcons'
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 import AccountNavigator from './AccountNavigator'
 import FeedNavigator from './FeedNavigator'
@@ -10,37 +12,54 @@ import routes from './routes'
 
 const Tab = createBottomTabNavigator()
 
-export default AppNavigator = () => (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen
-            name={routes.FEED}
-            component={FeedNavigator}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <Icon name="home" color={color} size={size} />
-                ),
-            }}
-        />
-        <Tab.Screen
-            name={routes.LISTING_EDIT}
-            component={ListingEditScreen}
-            options={({ navigation }) => ({
-                tabBarButton: () => (
-                    <NewListingButton
-                        onPress={() => navigation.navigate(routes.LISTING_EDIT)}
-                    />
-                ),
-            })}
-        />
-        <Tab.Screen
-            name={routes.ACCOUNT_NAVIGATOR}
-            component={AccountNavigator}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <Icon name="account" color={color} size={size} />
-                ),
-                title: 'Account'
-            }}
-        />
-    </Tab.Navigator>
-)
+export default AppNavigator = () => {
+    useEffect(() => {
+        registerForPushNotifications();
+    }, []);
+
+    const registerForPushNotifications = async () => {
+        const { granted } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        if (!granted) return;
+
+        try {
+            const token = await Notifications.getExpoPushTokenAsync();
+        } catch (error) {
+            console.log("Erorr getting the push token", error);
+        }
+    };
+
+    return (
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen
+                name={routes.FEED}
+                component={FeedNavigator}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="home" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name={routes.LISTING_EDIT}
+                component={ListingEditScreen}
+                options={({ navigation }) => ({
+                    tabBarButton: () => (
+                        <NewListingButton
+                            onPress={() => navigation.navigate(routes.LISTING_EDIT)}
+                        />
+                    ),
+                })}
+            />
+            <Tab.Screen
+                name={routes.ACCOUNT_NAVIGATOR}
+                component={AccountNavigator}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="account" color={color} size={size} />
+                    ),
+                    title: 'Account'
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
