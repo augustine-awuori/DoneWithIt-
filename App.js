93942bf0fd140861760cfe33f948766820f0e262
Augustine Awuori <augustineawuori95@gmail.com>
2022-05-20 16:59:29 +0300

    Hide and show splash screen when app is ready and not respectively.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { navigationRef } from './app/navigation/rootNavigation';
 import AppNavigator from './app/navigation/AppNavigator';
@@ -11,10 +12,13 @@ import OfflineNotice from './app/components/OfflineNotice';
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreUser = async () => {
+    await SplashScreen.preventAutoHideAsync();
     const user = await authStorage.getUser();
     if (user) setUser(user);
+    await SplashScreen.hideAsync();
   };
 
   useEffect(() => {
